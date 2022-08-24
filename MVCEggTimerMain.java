b55b288db07332640dcd2a55a51b67b69cff7cc9
19x89 <33625931+19x89@users.noreply.github.com>
2022-08-24 22:47:43 +0200

    Create MVCEggTimerMain.java

---
+++ MVCEggTimerMain.java
@@ -0,0 +1,43 @@
+package egg_timer;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class MVCEggTimerMain {
+    protected EggTimerModel eggTimerModel;
+    protected EggTimerCanvas eggTimerCanvas;
+    protected Frame frame;
+    protected Panel buttonPanel;
+    protected static Button startButton;
+    
+    public MVCEggTimerMain() {
+        eggTimerModel = new EggTimerModel( 10 );
+        eggTimerCanvas = new EggTimerCanvas( eggTimerModel );
+        frame = new Frame( "Kurzzeitwecker" );
+        frame.add( eggTimerCanvas, BorderLayout.CENTER );
+        
+        startButton = new Button( "Start" );
+        buttonPanel = new Panel();
+        buttonPanel.add( startButton );
+        frame.add( buttonPanel, BorderLayout.SOUTH );
+        
+        EggTimerController eggTimerController = new EggTimerController( eggTimerModel, eggTimerCanvas );
+        
+        startButton.addActionListener( eggTimerController );
+        
+        frame.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing( WindowEvent e ) {
+                System.exit( 0 );
+            }
+        });
+        
+        frame.pack();
+        frame.setLocationRelativeTo( null );
+        frame.setVisible( true );
+    }
+    
+    public static void main( String[] args ) {
+        new MVCEggTimerMain();
+    }
+}
