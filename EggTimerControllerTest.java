4873b5313c9a3a2760a6e448c6e23a615093ac89
19x89 <33625931+19x89@users.noreply.github.com>
2022-08-24 12:55:02 +0200

    Update EggTimerControllerTest.java

--- EggTimerControllerTest.java
+++ EggTimerControllerTest.java
@@ -4,11 +4,5 @@ public class EggTimerControllerTest {
     public static void main( String[] args ) {
         EggTimerModel eggTimerModel = new EggTimerModel( 100 );
         EggTimerController eggTimerController = new EggTimerController( eggTimerModel );
-        
-        /*EggTimerModel eggTimerModel2 = new EggTimerModel( 10 );
-        EggTimerController eggTimerController2 = new EggTimerController( eggTimerModel2 );
-        
-        EggTimerModel eggTimerModel3 = new EggTimerModel( 5 );
-        EggTimerController eggTimerController3 = new EggTimerController( eggTimerModel3 );*/
     }
 }
