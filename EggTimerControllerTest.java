6e76cfa1a825d357283a08288788795efdefb127
19x89 <33625931+19x89@users.noreply.github.com>
2022-08-24 22:44:31 +0200

    Delete EggTimerControllerTest.java

--- EggTimerControllerTest.java
+++
@@ -1,8 +0,0 @@
-package egg_timer;
-
-public class EggTimerControllerTest {
-    public static void main( String[] args ) {
-        EggTimerModel eggTimerModel = new EggTimerModel( 100 );
-        EggTimerController eggTimerController = new EggTimerController( eggTimerModel );
-    }
-}
